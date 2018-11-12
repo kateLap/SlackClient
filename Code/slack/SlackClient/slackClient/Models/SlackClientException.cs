9a2cc47e7a0a1c359f31ed0c9de13c32922bd46a
kateLap <36528817+kateLap@users.noreply.github.com>
2018-11-13 00:55:39 +0300

    Slack clien exception created

---
+++ Code/slack/SlackClient/slackClient/Models/SlackClientException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using SlackClient.Models.Response;
+
+namespace SlackClient.Models
+{
+    public class SlackClientException : Exception
+    {
+        public SlackClientException()
+        {
+        }
+
+        public SlackClientException(string message) : base(message)
+        {
+        }
+
+        public SlackClientException(string message, Exception innerException) : base(message, innerException)
+        {
+        }      
+
+        public SlackResponse Response { get; set; }
+    }
+}
