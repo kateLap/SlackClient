242b39ee414d92854f2cfcba439b0494cd50aa6a
kateLap <36528817+kateLap@users.noreply.github.com>
2018-11-13 00:58:52 +0300

    Snake case converter created

---
+++ Code/slack/SlackClient/slackClient/Models/SlackPropertyNamesContractResolver.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Serialization;
+
+namespace SlackClient.Models
+{
+    public class SlackPropertyNamesContractResolver : DefaultContractResolver
+    {
+        public SlackPropertyNamesContractResolver()
+          : base()
+        {
+        }
+
+        protected override string ResolvePropertyName(string propertyName)
+        {
+            return SnakeCaseUtils.ToSnakeCase(propertyName);
+        }
+    }
+}
---
+++ Code/slack/SlackClient/slackClient/Models/SnakeCaseUtils.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+
+namespace SlackClient.Models
+{
+        public static class SnakeCaseUtils
+        {
+            public static string ToSnakeCase(string name)
+            {
+                var sb = new StringBuilder(name);
+                int state = 0;
+                for (int i = 0; i < sb.Length; ++i)
+                {
+                    var c = sb[i];
+                    switch (state)
+                    {
+                        case 0:
+                            if (char.IsUpper(c))
+                            {
+                                sb[i] = char.ToLowerInvariant(c);
+                            }
+                            state = 1;
+                            break;
+                        case 1:
+                            if (char.IsUpper(c))
+                            {
+                                sb.Insert(i, '_');
+                                state = 0;
+                            }
+                            else if (char.IsDigit(c))
+                            {  
+                                sb.Insert(i, '_');
+                                state = 2;
+                            }
+                            break;
+                        case 2:
+                            if (char.IsUpper(c))
+                            {
+                                sb.Insert(i, '_');
+                                state = 0;
+                            }
+                            break;
+                    }
+                }
+                return sb.ToString();
+            }
+        }
+}
