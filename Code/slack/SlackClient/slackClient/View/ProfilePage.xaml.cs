7815ad6a24092c1e45b445cb1e36eb11958b7b69
kateLap <36528817+kateLap@users.noreply.github.com>
2018-11-14 00:56:29 +0300

    Profile page created

---
+++ Code/slack/SlackClient/slackClient/View/ProfilePage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using SlackClient.ViewModels;
+using Xamarin.Forms;
+
+namespace SlackClient.Views
+{
+	public partial class ProfilePage : ContentPage
+	{
+		public ProfilePage ()
+		{
+			InitializeComponent ();
+            this.BindingContext = new ProfileViewModel(this);
+        }
+	}
+}
