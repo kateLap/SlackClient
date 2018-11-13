7ee7f54793514955693862c38a4bcaa266b5a786
kateLap <36528817+kateLap@users.noreply.github.com>
2018-11-14 00:17:05 +0300

    Base types created

---
+++ Code/slack/SlackClient/slackClient/Models/ChatMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SlackClient.Models
+{
+    public class ChatMessage
+    {
+        public string Text { get; set; }
+
+        public string UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string UserImage { get; set; }
+
+        public string Time { get; set; }
+    }
+}
---
+++ Code/slack/SlackClient/slackClient/Models/UsersChannel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SlackClient.Models
+{
+    public class UsersChannel
+    {
+       public string Name { get; set; }
+       public string Id { get; set; }
+    }
+}
