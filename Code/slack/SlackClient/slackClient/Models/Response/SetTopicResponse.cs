4a34f3bc2ef80d3b3eae576200e772cf7efa4832
kateLap <36528817+kateLap@users.noreply.github.com>
2018-11-14 18:18:42 +0300

    New response type created

---
+++ Code/slack/SlackClient/slackClient/Models/Response/SetTopicResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SlackClient.Models.Response
+{
+    public class SetTopicResponse : SlackResponse
+    {
+        public string Topic { get; set; }
+    }
+}
